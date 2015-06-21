d3c02b918be152af085a8e545fee6c59adb1d07f
jhackwell <jhackwell@gmail.com>
2015-06-20 20:09:04 -0700

    Removed transport as a configuration option (only HTTPS is allowed by the API).  Fixed HTML encoding of slugs.  Added iGET method.

--- rito.js
+++ rito.js
@@ -3,6 +3,7 @@
  */
 
 var mustache = require('mustache');
+var https = require('https');
 var _ = require('lodash');
 
 var Client = function (settings) {
@@ -10,10 +11,21 @@ var Client = function (settings) {
 
   this.buildURI = function (slug) {
     return mustache.render(
-      '{{api.transport}}://{{api.region}}.{{api.base}}/{{slug}}?api_key={{api.key}}',
-      _.merge({"slug": slug}, this.settings)
+      // The ampersand in the slug is to prevent slashes from being HTML encoded
+      'https://{{api.region}}.{{api.base}}/{{& slug}}?api_key={{api.key}}',
+      // We render the slug through mustache so that it can access settings as well.
+      _.merge({"slug": mustache.render(slug, this.settings)}, this.settings)
     );
   };
+
+  /*
+   * Get is the only method supporting by the RIOT API at this time,
+   * and HTTPS is the only transport.
+   */
+  // todo don't take slug, take route name or something like that
+  this.get = function (slug, err, res) {
+    https.get(this.buildURI(slug), res).on('error', err);
+  }
 };
 
 exports.Client = Client;
