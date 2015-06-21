85f1356c8d39b234e3172a7b7b36a01dc3d20db0
jhackwell <jhackwell@gmail.com>
2015-06-20 23:32:37 -0700

    JSHint sure is whiny.

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 'use strict';
 var assert = require('assert');
-var rito = require('../lib');
+//var rito = require('../lib');
 
 describe('rito', function () {
   it('should have unit test!', function () {
