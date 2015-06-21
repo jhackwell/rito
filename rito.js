e0feb9abb96bf534b5eb719f9eb09ae0b6f03fee
jhackwell <jhackwell@gmail.com>
2015-06-21 01:46:16 -0700

    Switched to Chai spies for callbacks in Mocha tests.  Changed client to clone added aliases.  Abstracted slug transformation stuff for testing.

--- rito.js
+++ rito.js
@@ -4,10 +4,16 @@
 'use strict';
 
 var mustache = require('mustache');
-var https = require('https');
 var _ = require('lodash');
 
-var Client = function (settings) {
+/**
+ * todo Description
+ *
+ * @param settings Configuration object.
+ * @param https Either the Node.js https module, or a mock of it for testing.
+ * @constructor
+ */
+var Client = function (settings, https) {
   this.settings = settings;
   this.aliases = settings.aliases ? settings.aliases : [];
 
@@ -23,7 +29,9 @@ var Client = function (settings) {
       return oldAlias.name == alias.name
     });
     if (!existing) {
-      this.aliases.push(alias);
+      // We clone this rather than just pushing it on because the referent may have
+      // been previously added but later mutated at some point.
+      this.aliases.push(_.clone(alias));
       res({msg: mustache.render('Added alias for name {{name}}', alias)})
     } else {
       if (existing.route == alias.route) {
@@ -39,14 +47,26 @@ var Client = function (settings) {
   };
 
   /**
-   * Call a previously-registered alias for a fully-qualified, versioned route endpoint.
+   *
+   * @param alias
+   * @param method
+   * @param params
+   * @param err
+   * @param res
+   */
+  this.call = function (alias, method, params, err, res) {
+
+  };
+
+  /**
+   * Build slug from previously-registered route alias and params.
    *
    * @param alias String Name of a previously-registered alias
    * @param params Object Key-value pairs of parameter names and values
    * @param err Error callback
    * @param res Result callback
    */
-  this.call = function (alias, params, err, res) {
+  this._buildSlug = function (alias, params, err, res) {
     var route = _.find(this.aliases, function (existing) {
       return existing.name == alias
     });
@@ -64,10 +84,10 @@ var Client = function (settings) {
       } else {
         // We have a route, and we have all its parameters.  Now it's just a simple
         // matter of rendering the slug and shooting it at Rito.
-        var slug = mustache.render(route.route, params);
-        this._get(slug, err, res)
+        res({slug: mustache.render(route.route, params)});
       }
     } else {
+      console.log('here is the error');
       err({msg: 'Attempted to call unregistered alias with name ' + alias})
     }
   };
--- test/rito-test.js
+++ test/rito-test.js
@@ -2,63 +2,95 @@
  * Created by jay on 6/20/2015.
  */
 'use strict';
-var assert = require('chai').assert;
+
+// Hey, that's us!
 var rito = require('../rito.js');
 
-describe('registerAlias', function () {
-  before(function () {
-    this.noErr = function (err) {
-      // This way we get the error message in the Mocha logs if the
-      // assertion fails.
-      if (err) {
-        assert.notOk(err.msg)
-      }
-    };
-  });
+// A bunch of Chai stuff.
+// Webstorm detects 'should' and 'expect' as unused, but they're actually necessary
+// for demeter chained spy assertions.
+//noinspection JSUnusedGlobalSymbols
+var chai = require('chai'),
+  assert = chai.assert,
+  should = chai.should(),
+  expect = chai.expect,
+  spies = require('chai-spies');
 
+chai.use(spies);
+
+// Convenience method for generating spies that assert the res/err message
+// matches a particular string
+var regexSpy = function (str) {
+  return chai.spy(function (res) {
+      var re = new RegExp(str);
+      assert.ok(re.exec(res.msg))
+    }
+  )
+};
+
+describe('registerAlias', function () {
   beforeEach(function () {
     this.rito = new rito.Client({
-      api: {key: 'key', base: 'base', region: 'region'}
+      api: {key: 'key', base: 'base', region: 'region'},
+      https: {}
     });
   });
 
   it('should add a new route alias', function () {
-    this.rito.registerAlias(
-      {name: 'foo'},
-      this.noErr,
-      function (res) {
-        assert.ok(/Added alias/.exec(res.msg))
-      }
-    );
+    var errSpy = chai.spy();
+    var addedSpy = regexSpy('Added alias');
+
+    this.rito.registerAlias({name: 'foo'}, errSpy, addedSpy);
+
+    addedSpy.should.have.been.called.once();
+    errSpy.should.not.have.been.called();
   });
 
   it('should ignore identical collisions', function () {
+    var errSpy = chai.spy();
+    var resSpy = chai.spy();
+    var dupSpy = regexSpy('Duplicate alias');
+
+    // We should get success the first time...
     var alias = {name: 'foo', route: 'bar'};
-    this.rito.registerAlias(alias, this.noErr, function () {
-    });
-    this.rito.registerAlias(
-      alias,
-      this.noErr,
-      function (res) {
-        assert.ok(/Duplicate alias/.exec(res.msg))
-      }
-    );
-  })
-  ;
-  it('should error if a conflicting alias is attempted to be registered', function () {
-    var alias = {name: 'foo', route: 'bar'};
-    this.rito.registerAlias(alias, this.noErr, function () {
-    });
-    alias.route = 'baz';
-    this.rito.registerAlias(
-      alias,
-      function (err) {
-        assert.ok(/Attempted to register/.exec(err.msg))
-      },
-      function (res) {
-        // Res is undefined behavior for err, so we'll ignore.
-      }
-    );
+    this.rito.registerAlias(alias, errSpy, resSpy);
+
+    // and success with warning message about duplication here
+    this.rito.registerAlias(alias, errSpy, dupSpy);
+
+    resSpy.should.have.been.called.once();
+    dupSpy.should.have.been.called.once();
+    errSpy.should.not.have.been.called();
   });
 
+  it('should error if a conflicting alias is attempted to be registered', function () {
+    var errSpy = regexSpy('Attempted to register');
+    var resSpy = chai.spy();
+
+    // We should get success the first time...
+    var alias = {name: 'foo', route: 'bar'};
+    this.rito.registerAlias(alias, errSpy, resSpy);
+
+    // And an error the second time.
+    alias.route = 'baz';
+    this.rito.registerAlias(alias, errSpy, resSpy);
+
+    errSpy.should.have.been.called.once();
+    resSpy.should.have.been.called.once();
+  });
+});
+
+describe('_buildSlug', function () {
+  beforeEach(function () {
+    this.rito = new rito.Client({
+      api: {key: 'key', base: 'base', region: 'region'},
+      https: {}
+    });
+  });
+
+  it('should error if an attempt is made to transform an unregistered route', function () {
+    var errSpy = regexSpy('Attempted to call');
+
+    this.rito._buildSlug('foo', {}, errSpy);
+  });
 });
